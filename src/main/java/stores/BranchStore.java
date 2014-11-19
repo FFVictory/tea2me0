0c6b0aee2af4afb51ca462632e36dce26e9a8787
ffvictory <vasiljev.andrew@gmail.com>
2014-11-19 01:11:39 +0000

    Branch Store

---
+++ src/main/java/stores/BranchStore.java
@@ -0,0 +1,81 @@
+package stores;
+
+/**
+ * Created by Drew on 19/11/2014.
+ */
+public class BranchStore {
+    int branchId;
+    int size;
+    String address;
+    int expenditure;
+    int advertismentCosts;
+    int leaseCost;
+    int revenue;
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getExpenditure() {
+        return expenditure;
+    }
+
+    public void setExpenditure(int expenditure) {
+        this.expenditure = expenditure;
+    }
+
+    public int getAdvertismentCosts() {
+        return advertismentCosts;
+    }
+
+    public void setAdvertismentCosts(int advertismentCosts) {
+        this.advertismentCosts = advertismentCosts;
+    }
+
+    public int getLeaseCost() {
+        return leaseCost;
+    }
+
+    public void setLeaseCost(int leaseCost) {
+        this.leaseCost = leaseCost;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    int cityId;
+
+}
