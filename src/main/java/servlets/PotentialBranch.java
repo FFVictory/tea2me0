36b5471c79af6c83f2d4ee180b6bbe6368d3860b
ffvictory <vasiljev.andrew@gmail.com>
2014-11-20 22:37:48 +0000

    PotentialBranch Servlet

--- src/main/java/servlets/PotentialBranch.java
+++ src/main/java/servlets/PotentialBranch.java
@@ -1,11 +1,14 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Created by Drew on 20/11/2014.
@@ -17,6 +20,12 @@ public class PotentialBranch extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        models.PotentialBranch pbm = new models.PotentialBranch();
+        LinkedList<stores.PotentialBranch> potentialBranches = pbm.getBranches();
+        session.setAttribute("potentialBranch", potentialBranches);
+        RequestDispatcher rd = request.getRequestDispatcher("/potentialBranch.jsp");
+        rd.forward(request, response);
+        
     }
 }
