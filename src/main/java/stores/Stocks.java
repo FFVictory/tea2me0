cc4ad2352653818dbaa8aec972676c7068ec6d8c
ffvictory <vasiljev.andrew@gmail.com>
2014-11-21 12:17:56 +0000

    Stores with Views

--- src/main/java/stores/Stocks.java
+++ src/main/java/stores/Stocks.java
@@ -4,16 +4,43 @@ package stores;
  * Created by Andrew on 06/11/2014.
  */
 public class Stocks {
-    int branchId,teaType,stock,stockRequested;
+    int branchId;
+    int teaType;
+    int teaName;
 
-    public int getTeaType(){return teaType;}
-    public int getStock(){return stock;}
-    public int getBranchId(){return branchId;}
-    public int getStockRequested(){return stockRequested;}
-    public void setStockRequested(int stockReq){this.stockRequested=stockReq;}
-    public void setTeaType(int tea){this.teaType = tea; }
-    public void setStock(int stock){this.stock= stock; }
-    public void setBranchId(int  branchId){this.branchId= branchId; }
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public int getTeaType() {
+        return teaType;
+    }
+
+    public void setTeaType(int teaType) {
+        this.teaType = teaType;
+    }
+
+    public int getTeaName() {
+        return teaName;
+    }
+
+    public void setTeaName(int teaName) {
+        this.teaName = teaName;
+    }
+
+    public int getStockRequested() {
+        return stockRequested;
+    }
+
+    public void setStockRequested(int stockRequested) {
+        this.stockRequested = stockRequested;
+    }
+
+    int stockRequested;
 
 
 }
